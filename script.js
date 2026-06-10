81b956d99802c2b5e80f18661688390d0f8ff6f1
Pavlos <paulpapachrys@gmail.com>
2026-06-10 15:54:33 +0300

    Contact  Page Implementation

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.querySelector('#navbar-placeholder').innerHTML=`
 			<div class="container-fluid" id="navbar_container">
 
 				<div class="d-flex align-items-center">
-					<img src="logo.jpg" alt="Company Logo" width="50" height="50">
+					<img src="images/logo.jpg" alt="Company Logo" width="50" height="50">
 					<span class="company_name"> My Company</span>
 				</div>
 				
@@ -42,7 +42,7 @@ document.querySelector('#footer-placeholder').innerHTML=`
 		<div class="container-fluid d-flex align-items-center py-3 border-top" id="footer_container">
 
             <div class="d-flex align-items-center ms-5 gap-3">
-                <img src="logo.jpg" alt="Company Logo" width="50" height="50">
+                <img src="images/logo.jpg" alt="Company Logo" width="50" height="50">
                 <span class="company_name">My Company © 2026. All rights reserved.</span>
             </div>
 	
